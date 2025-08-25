242e7e4be4bffb6db5f7bc9f67ba9e3745fea920
Mohammed Tawfik <Mohamed.Tawfik2@bd.com>
2025-08-25 20:51:39 +0300

    Install and configure Convex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,19 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+'use client';
+
+import { useQuery } from 'convex/react';
+import { api } from '@workspace/backend/_generated/api';
 
 export default function Page() {
+  const users = useQuery(api.users.getAllUsers);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello App</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(1, 2)}</p>
+        {users && users.length === 0 && <div>No users</div>}
+        {users?.map((user) => (
+          <div key={user._id}>{user.name}</div>
+        ))}
       </div>
     </div>
   )
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,11 +1,19 @@
-import { Button } from "@workspace/ui/components/button"
+'use client';
+
+import { useQuery } from 'convex/react';
+import { api } from '@workspace/backend/_generated/api';
 
 export default function Page() {
+  const users = useQuery(api.users.getAllUsers);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Widget</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">Hello App</h1>
+        {users && users.length === 0 && <div>No users</div>}
+        {users?.map((user) => (
+          <div key={user._id}>{user.name}</div>
+        ))}
       </div>
     </div>
   )
